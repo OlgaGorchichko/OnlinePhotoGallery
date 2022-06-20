d09afa5265b1644e8112a6f4f6e517f686838506
Olga <olga@gorchichko.com>
2022-06-20 13:21:55 +0300

    fixed bag with logout

--- scripts/authorization.js
+++ scripts/authorization.js
@@ -54,6 +54,8 @@ const logout = (e) => {
         localStorage.removeItem('Bearer');
         btn.textContent = '';
         btn.style.backgroundImage = '';
+        btn.removeEventListener('click', logout);
+        btn.addEventListener('click', login);
     }
 }
 
